374a04eb468a3699fe263abe3862753c4cf14903
Pravles Redneckoff <me@pravles.com>
2025-10-10 06:55:57 +0200

    ctl-app, execution engineering, MVP 1

--- src/main/resources/clj/compose-time-slot-data.clj
+++ src/main/resources/clj/compose-time-slot-data.clj
@@ -72,14 +72,18 @@
           (traverse-days
             first-day
             last-day
-            #(compose-time-slots-for-day rules-by-day-of-week %1 %2))]
+            #(compose-time-slots-for-day rules-by-day-of-week %1 %2))
+        time-slots (:time-slots days-traversal-result)
+
+        ]
     (println "compose-time-slot-data")
     (println "first-day: " first-day)
     (println "number-of-weeks-to-schedule: " number-of-weeks-to-schedule)
     (println "last-day: " last-day)
     (println "from-through-rules: " from-through-rules)
     (println "rules-by-day-of-week: " rules-by-day-of-week)
-    (.put ctx "time-slots" (:time-slots days-traversal-result))
+    (println "time-slots: " time-slots)
+    (.put ctx "time-slots" time-slots)
     ctx))
 
 (defn traverse-days
